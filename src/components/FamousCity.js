bff3d0cb8e935c970498fdfff5d4ecd6cc6692de
alicbm <abarandica1234@gmail.com>
2022-12-19 13:10:44 -0500

    optimization for medium and long views

---
+++ src/components/FamousCity.js
@@ -0,0 +1,16 @@
+import '../styles/FamousCity.css';
+
+export function FamousCity(){
+  return(
+    <div className="FamousCity">
+      <h2 className="FamousCity__title">Famous City</h2>
+      <div className="FamousCity__content">
+        <div className='FamousCity__content-city'></div>
+        <div className='FamousCity__content-city'></div>
+        <div className='FamousCity__content-city'></div>
+        <div className='FamousCity__content-city'></div>
+        <div className='FamousCity__content-city'></div>
+      </div>
+    </div>
+  )
+}
